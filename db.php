5baada1b1f872db3524238f2c435af39fd537b80
Ayush <ayushkumarnov1@gmail.com>
2026-05-11 12:30:20 +0530

    Fix: Remove closing PHP tag to prevent header output issues

--- db.php
+++ db.php
@@ -300,5 +300,4 @@ function haversine($lat1,$lng1,$lat2,$lng2) {
     $a = sin($dLat/2)*sin($dLat/2)+cos(deg2rad($lat1))*cos(deg2rad($lat2))*sin($dLng/2)*sin($dLng/2);
     return $R * 2 * atan2(sqrt($a), sqrt(1-$a));
 }
-?>
 
